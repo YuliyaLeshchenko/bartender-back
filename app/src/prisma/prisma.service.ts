4b9cfc35432606dfd1e134843e921a1ffd7302c7
Yuliya Leshchanka <y.leshchank@gmail.com>
2024-06-16 02:18:35 -0400

    [dev] tag type shown

--- app/src/prisma/prisma.service.ts
+++ app/src/prisma/prisma.service.ts
@@ -7,7 +7,8 @@ export class PrismaService extends PrismaClient {
         super({
             datasources: {
                 db: {
-                    url: "postgresql://postgres:root1@localhost:5434/bartender?schema=public"
+                    // url: "postgresql://postgres:root1@localhost:5434/bartender?schema=public"
+                    url: "postgresql://postgres:root1@10.142.0.2:5434/bartender?schema=public"
                 }
             }
         })
