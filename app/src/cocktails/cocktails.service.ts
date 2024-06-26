85c6a80fdfc0d72e670a39a5e058a672ba24db55
Yuliya Leshchanka <y.leshchank@gmail.com>
2024-06-26 17:45:55 -0400

    [fix] toFixed

--- app/src/cocktails/cocktails.service.ts
+++ app/src/cocktails/cocktails.service.ts
@@ -286,7 +286,7 @@ export class CocktailsService {
             }
         });
 
-        return {...cocktail, rating: cocktail.rating.toFixed(1)};
+        return {...cocktail, rating: (Number(cocktail.rating) ?? 0).toFixed(1)};
     }
 
     async getCocktailsByName(name: string = "") {
